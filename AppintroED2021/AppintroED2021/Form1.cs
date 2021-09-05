48491d3183534dd459754f77c6d88550f76b58ec
RaquelMMtz <70656439+RaquelMMtz@users.noreply.github.com>
2021-09-04 22:47:59 -0500

    Add files via upload

---
+++ AppintroED2021/AppintroED2021/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppintroED2021
+{
+    public partial class Form1 : Form
+    {
+        string dato;
+        int i = 0;
+        //string [] arreglo = new string [10]
+        Persona[] arreglo = new Persona[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            arreglo[i] = new Persona();
+            arreglo[i].Numero = i + 1;
+            arreglo[i].Nombre = textBox1.Text;
+            Random x = new Random();
+            arreglo[i].Edad = x.Next(1, 99);
+            textBox1.Clear();
+            i++;
+        }
+    }
+}
---
+++ AppintroED2021/AppintroED2021/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppintroED2021
+{
+    class Persona
+    {
+        int numero;
+        string nombre;
+        int edad;
+
+        public int  Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+        public override string ToString()
+        {
+            return numero + "" + nombre;
+        }
+    }
+}
